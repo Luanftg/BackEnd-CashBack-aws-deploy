bbf687abd9b304e6334164037958ff31cbc0a7bf
Luanftg <luanftgimenez@gmail.com>
2022-07-17 09:14:17 -0300

    incluindo PORT 3000

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express, { json } from 'express';
 import routes from './src/routes/index.js';
 import 'dotenv/config';
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+//const PORT = process.env.PORT;
+//const HOST = process.env.HOST;
+const PORT = 3000;
 
 const app = express();
 app.use(express.json());
