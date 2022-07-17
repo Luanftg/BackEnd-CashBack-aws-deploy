a49866b67c5b8dd2d1af35d3f217bd84618399bf
Luanftg <luanftgimenez@gmail.com>
2022-07-17 20:28:05 -0300

    version of heroku

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import routes from './src/routes/index.js';
 import 'dotenv/config';
 
 const PORT = process.env.PORT  || 3000;
-//const HOST = process.env.HOST;
+const HOST = process.env.HOST;
 //const PORT = 3000;
-const HOST = '';
+//const HOST = '';
 
 const app = express();
 app.use(express.json());
--- src/controllers/HookConfig.js
+++ src/controllers/HookConfig.js
@@ -3,14 +3,18 @@ import 'dotenv/config';
 const hookConfigController = {
     ping: (req, res) => {
     
-        const { appkey } = req.headers;        
+        //const { appkey } = req.headers;   
+        console.log(req.body);    
+        const { hookConfig } = req.body;        
+        console.log(hookConfig); 
 
         //const passKey = process.env.appKey;
-        
+        const appkey = hookConfig;
         if(!appkey) {
           res.status(422).json({"message": "Você precisa enviar uma senha!" });
         } else {
-          const passKey = 'vtex-bitsized';
+          //const passKey = 'vtex-bitsized';
+          const passKey = 'ping';
           if( appkey === passKey){
             res.sendStatus(200);
           }else {
