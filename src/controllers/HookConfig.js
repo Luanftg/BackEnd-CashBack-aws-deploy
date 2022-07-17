607fe2609e1c169e52bb7674f27ab6fbc7367fd1
Luanftg <luanftgimenez@gmail.com>
2022-07-17 09:02:59 -0300

    verifca se há chave no req.headers

--- src/controllers/HookConfig.js
+++ src/controllers/HookConfig.js
@@ -5,13 +5,19 @@ const hookConfigController = {
     
         const { appkey } = req.headers;        
 
-        const passKey = process.env.appKey;
-
-        if( appkey === passKey){
-          res.sendStatus(200);
-        }else{
-          res.status(422).json({"message": "Senha inválida!"});
+        //const passKey = process.env.appKey;
+        
+        if(!appkey) {
+          res.status(422).json({"message":"Você precisa enviar uma senha!"});
+        } else {
+          const passKey = 'vtex-bitsized';
+          if( appkey === passKey){
+            res.sendStatus(200);
+          }else {
+            res.status(422).json({"message": "Senha inválida!"});
+          }
         }
+
       }
 }
 
