7a54fc41ad9dbfce78ebdeca5993bc584458eaa9
Luanftg <luanftgimenez@gmail.com>
2022-07-17 21:04:21 -0300

    update hook

--- src/controllers/HookConfig.js
+++ src/controllers/HookConfig.js
@@ -1,22 +1,23 @@
 import 'dotenv/config';
-
+const OrderIds = [];
 const hookConfigController = {
     ping: (req, res) => {
-    
-        //const { appkey } = req.headers;   
-        console.log(req.body);    
-        const { hookConfig } = req.body;        
-        console.log(hookConfig); 
+         
+        const { hookConfig } = req.body;
+        const { OrderId }  = req.body;   
 
-        //const passKey = process.env.appKey;
-        const appkey = hookConfig;
-        if(!appkey) {
-          res.status(422).json({"message": "Você precisa enviar uma senha!" });
+        if(!hookConfig && !OrderId) {
+          //res.status(422).json({"message": "Você precisa enviar uma senha!" });  
+          res.status(422).json({"message": "Bad Request!!!" });  
+        } else if (OrderId) { 
+          OrderIds.push(OrderId);
+          res.sendStatus(201);
+
         } else {
-          //const passKey = 'vtex-bitsized';
-          const passKey = 'ping';
-          if( appkey === passKey){
+          //const passKey = 'ping';
+          if( hookConfig === 'ping'){
             res.sendStatus(200);
+
           }else {
             res.status(422).json({"message": "Senha inválida!"});
           }
