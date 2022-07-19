a57018a151b7f5ea0c0b430311cc3de50027d3e1
Luanftg <luanftgimenez@gmail.com>
2022-07-19 10:14:43 -0300

    add fetch for OrderAPI

--- src/controllers/HookConfig.js
+++ src/controllers/HookConfig.js
@@ -1,18 +1,36 @@
 import 'dotenv/config';
-const OrderIds = [];
+import fetch from 'node-fetch';
+
 const hookConfigController = {
     ping: (req, res) => {
          
         const { hookConfig } = req.body;
-        const { OrderId }  = req.body;   
+        const { OrderId }  = req.body;  
 
         if(!hookConfig && !OrderId) {
-          //res.status(422).json({"message": "Você precisa enviar uma senha!" });  
           res.status(422).json({"message": "Bad Request!!!" });  
-        } else if (OrderId) { 
-          OrderIds.push(OrderId);
-          console.log(OrderIds);
-          res.sendStatus(201);
+          } else if (OrderId) { 
+
+            const OrderAPIPath = `https://bitsized.myvtex.com/api/oms/pvt/orders/${OrderId}/`;
+            const OrderHeader = {
+                "Content-Type": "application/json",
+                "Connection": "keep-alive",
+                "X-VTEX-API-AppKey": "vtexappkey-bitsized-FEQYTP",
+                "X-VTEX-API-AppToken":"SGLRMYLVUYUVRTOOJGAPSUBTBZIMODTEJTETGNPBCLMBEMXFPMXTKVTDLWCFUEHDNJUJNPODRWLDWFVZDGKSONLCIINAXTIHUYOECVFEMZIGLWNKLMMVEAVVDYAVUNAV",
+            }
+            fetch(OrderAPIPath, {
+              method:'GET',
+              headers: OrderHeader
+            })
+              .then(function(response){
+                return response.json();
+              })
+              .then(function(response){
+                console.log(response);
+                const { value } = response;
+                console.log(value);
+              })
+            res.sendStatus(201);
 
         } else {
           //const passKey = 'ping';
